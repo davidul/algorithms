988c4397e0260b19689211871cf24102bf730603
DAVIDUL <ulicny.david@gmail.com>
2020-12-18 12:53:52 +0100

    Fibonacci

--- src/main/java/davidul/utils/Fibonacci.java
+++ src/main/java/davidul/utils/Fibonacci.java
@@ -1,2 +1,22 @@
-package davidul.utils;public class Fibonacci {
+package davidul.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibonacci {
+    private static final Map<Integer, Integer> memo = new HashMap<>();
+
+    /**
+     * Memoized Fibonacci number
+     * @param n n-th F-number
+     * @return Fibonacci number
+     */
+    public static int fib(int n){
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        if(n <= 2) return 1;
+        memo.put(n, fib(n-2) + fib(n-1));
+        return memo.get(n);
+    }
 }
--- src/test/java/davidul/utils/FibonacciTest.java
+++ src/test/java/davidul/utils/FibonacciTest.java
@@ -1,4 +1,16 @@
-import static org.junit.Assert.*;
+package davidul.utils;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FibonacciTest {
-  
+
+    //1 1 2 3 5 8
+    @Test
+    public void fib() {
+        assertThat(Fibonacci.fib(1)).isEqualTo(1);
+        assertThat(Fibonacci.fib(6)).isEqualTo(8);
+        assertThat(Fibonacci.fib(40)).isEqualTo(102334155); //102334155
+    }
 }
